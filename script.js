f513b264732bf7ca4c812fe9ac7daf9712641d1b
pmacaluso3 <moxious.manoeuvre@gmail.com>
2021-03-15 12:40:12 -0400

    add stretch goals

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
-document.querySelector('#show-users').addEventListener('click', () => {
-  fetch("https://randomuser.me/api?results=5").then((response) => {
+const createUser = (userObj) => {
+  const userDiv = document.createElement('div')
+  userDiv.classList.add('user')
+  
+  const userName = document.createElement('h4')
+  userName.innerText = user.name.first
+  userName.classList.add('user-name')
+  userDiv.append(userName)
+  
+  const userEmail = document.createElement('p')
+  userEmail.innerText = user.email
+  userEmail.classList.add('user-email')
+  userDiv.append(userEmail)
+  
+  const userImg = document.createElement('img')
+  userImg.src = user.picture.medium
+  userImg.classList.add('user-image')
+  userDiv.append(userImg)
+
+  document.querySelector('#users').append(userDiv)
+}
+
+const fetchAndCreate = () => {
+  const count = document.querySelector('#users-count').value || 5
+
+  fetch(`https://randomuser.me/api?results=${count}`).then((response) => {
     response.json().then((json) => {
       for (user of json.results) {
-        
-        const userName = document.createElement('h4')
-        userName.innerText = user.name.first
-        userName.classList.add('user-name')
-        document.querySelector('#users').append(userName)
-        
-        const userEmail = document.createElement('p')
-        userEmail.innerText = user.email
-        userEmail.classList.add('user-email')
-        document.querySelector('#users').append(userEmail)
-  
-        const userImg = document.createElement('img')
-        userImg.src = user.picture.medium
-        userImg.classList.add('user-image')
-        document.querySelector('#users').append(userImg)
-  
+        createUser(user)
       }
     })
   })
+}
+
+document.querySelector('#show-users').addEventListener('click', () => {
+  document.querySelector('#users').textContent = ''
+  fetchAndCreate()
 })
+
+fetchAndCreate()
 
 
